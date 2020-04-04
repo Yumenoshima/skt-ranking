55ed2151cd78c99ccbd8541fe7a27bca474291a7
Yumenoshima EDEN/占拠マンSKT豆腐屋 <skt.tofuya@gmail.com>
2020-04-04 10:28:08 +0900

    changed layout a little

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import Table from 'react-bootstrap/Table';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
@@ -27,68 +30,83 @@ function App() {
         <div className="App">
             <header className="App-header">
 
-                <Dropdown onSelect={function (evt) {
-                    setServer(evt)
-                }}>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                        サーバー選択
-                    </Dropdown.Toggle>
+                <Container>
+                    <Row>
+                        <Col>
 
-                    <Dropdown.Menu>
-                        <Dropdown.Item eventKey="1">デポ01</Dropdown.Item>
-                        <Dropdown.Item eventKey="2">デポ02</Dropdown.Item>
-                        <Dropdown.Item eventKey="3">デポ03</Dropdown.Item>
-                        <Dropdown.Divider/>
+                            <Dropdown onSelect={function (evt) {
+                                setServer(evt)
+                            }}>
+                                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                    サーバー選択
+                                </Dropdown.Toggle>
 
-                        <Dropdown.Item eventKey="4">ケン01</Dropdown.Item>
-                        <Dropdown.Item eventKey="5">ケン02</Dropdown.Item>
-                        <Dropdown.Item eventKey="6">ケン03</Dropdown.Item>
-                        <Dropdown.Divider/>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item eventKey="1">デポ01</Dropdown.Item>
+                                    <Dropdown.Item eventKey="2">デポ02</Dropdown.Item>
+                                    <Dropdown.Item eventKey="3">デポ03</Dropdown.Item>
+                                    <Dropdown.Divider/>
 
-                        <Dropdown.Item eventKey="7">イシ01</Dropdown.Item>
-                        <Dropdown.Item eventKey="8">イシ02</Dropdown.Item>
-                        <Dropdown.Item eventKey="9">イシ03</Dropdown.Item>
-                        <Dropdown.Divider/>
+                                    <Dropdown.Item eventKey="4">ケン01</Dropdown.Item>
+                                    <Dropdown.Item eventKey="5">ケン02</Dropdown.Item>
+                                    <Dropdown.Item eventKey="6">ケン03</Dropdown.Item>
+                                    <Dropdown.Divider/>
 
-                        <Dropdown.Item eventKey="10">ケレ01</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
+                                    <Dropdown.Item eventKey="7">イシ01</Dropdown.Item>
+                                    <Dropdown.Item eventKey="8">イシ02</Dropdown.Item>
+                                    <Dropdown.Item eventKey="9">イシ03</Dropdown.Item>
+                                    <Dropdown.Divider/>
 
-                <h1>{serverName}</h1>
+                                    <Dropdown.Item eventKey="10">ケレ01</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
 
-                <img src={logo} className="App-logo" alt="logo"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h1>{serverName}</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <img src={logo} className="App-logo" alt="logo"/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Table striped bordered hover variant="dark">
+                                <thead>
+                                <tr>
+                                    <th>ランク</th>
+                                    <th>名前</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {data.map((item, index) => (
+                                    <tr>
+                                        <td>{item.ranking}</td>
+                                        <td>{item.name}</td>
+                                    </tr>
 
-
-                <Table striped bordered hover variant="dark">
-                    <thead>
-                    <tr>
-                        <th>ランク</th>
-                        <th>名前</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((item, index) => (
-                        <tr>
-                            <td>{item.ranking}</td>
-                            <td>{item.name}</td>
-                        </tr>
-
-                    ))}
-                    </tbody>
-                </Table>
-
-                <p>
-                    リネージュM記者クラブ<br/>
-                    <code>Twitter特派員募集中！</code>
-                </p>
-                <a
-                    className="App-link"
-                    href="https://twitter.com/Yumenoshima1"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Yumenoshima
-                </a>
+                                ))}
+                                </tbody>
+                            </Table>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <a
+                                className="App-link"
+                                href="https://twitter.com/Yumenoshima1"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                リネージュM記者クラブ
+                            </a>
+                        </Col>
+                    </Row>
+                </Container>
             </header>
         </div>
     );
